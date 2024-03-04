73307f15b11e444b16c7b4dbd0b773932a852109
ZEHAD <zehad146@gmail.com>
2024-03-05 03:06:49 +0600

    Create main.kt
    base64 kotlin

---
+++ main.kt
@@ -0,0 +1,14 @@
+import java.io.File
+import java.util.*
+
+fun main() {
+    val imagePath = "path/to/image.jpg"
+    val base64String = encodeFileToBase64Binary(imagePath)
+    println(base64String)
+}
+
+fun encodeFileToBase64Binary(filePath: String): String {
+    val file = File(filePath)
+    val bytes = file.readBytes()
+    return Base64.getEncoder().encodeToString(bytes)
+}
