3308e71b0bd94486d95069f2ff00a66129f4659a
zehadkhan <zehad146@gmail.com>
2024-03-05 03:39:03 +0600

    message added
    done

--- main.kt
+++ main.kt
@@ -5,7 +5,7 @@ fun main() {
     val imagePath = "path/to/image.jpg"
     val base64String = encodeFileToBase64Binary(imagePath)
     println(base64String)
-    println("Conversion done,complte file.")
+    println("Conversion done,complte file")
 }
 
 fun encodeFileToBase64Binary(filePath: String): String {
